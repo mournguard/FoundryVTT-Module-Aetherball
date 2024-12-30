ed6c2cbd1d6ab2a60d0f718becbf469e81da8da4
Nicola Baribeau <nicolabaribeau@gmail.com>
2024-12-30 01:29:50 -0500

    Insane

--- scripts/module.js
+++ scripts/module.js
@@ -1,8 +1,5 @@
-// Basically all of this stolen from Dice Tray cause I can't find a single explanation on how fvtt works : https://github.com/mclemente/fvtt-dice-tray
-
-const { ApplicationV2: ApplicationV2$1, HandlebarsApplicationMixin: HandlebarsApplicationMixin$1 } = foundry.applications.api;
-
-const ACTIVE_TILE = {Grass: 0, Dirt: 1, Stone: 2}
+// Much of this is taken from Dice Tray cause I can't find a single good explanation on how fvtt works : https://github.com/mclemente/fvtt-dice-tray
+const ACTIVE_TILE = {Invalid: -1, Grass: 0, Dirt: 1, Stone: 2}
 
 const TILE_MAP = {
 	[ACTIVE_TILE.Grass]: [
@@ -26,12 +23,39 @@ const TILE_MAP = {
 
 class Aetherball {
     popout
-	state = ACTIVE_TILE.Grass
+	current_tile = ACTIVE_TILE.Grass
+	scene_name = "Scene"
+	position = {
+		width: "auto"
+	}
+	grid_start = {x: 10, y: 1}
+	grid_size = {x: 41, y: 17}
+	grid_pattern = [[ACTIVE_TILE.Dirt,ACTIVE_TILE.Grass],[ACTIVE_TILE.Grass,ACTIVE_TILE.Stone]]
 
 	get settings() {
 		return {};
 	}
 }
+
+function validateTokenTile(t) {
+	if(!t || !t.inCombat || !t.scene.name == CONFIG.AETHERBALL.scene_name) return ACTIVE_TILE.Invalid
+	let tile_position = {
+		"x": (t.document._source.x - t.scene.dimensions.sceneRect.x) / t.scene.dimensions.distancePixels / t.scene.dimensions.distance,
+		"y": (t.document._source.y - t.scene.dimensions.sceneRect.y) / t.scene.dimensions.distancePixels / t.scene.dimensions.distance,
+	}
+	if(
+		CONFIG.AETHERBALL.grid_start.x < tile_position.x < CONFIG.AETHERBALL.grid_start.x + CONFIG.AETHERBALL.grid_size.x &&
+		CONFIG.AETHERBALL.grid_start.y < tile_position.y < CONFIG.AETHERBALL.grid_start.y + CONFIG.AETHERBALL.grid_size.y
+	) {
+		const patternWidth = CONFIG.AETHERBALL.grid_pattern[0].length;
+		const patternHeight = CONFIG.AETHERBALL.grid_pattern.length;
+		const mappedX = Math.abs(tile_position.x - CONFIG.AETHERBALL.grid_start.x) % patternWidth;
+		const mappedY = Math.abs(tile_position.y - CONFIG.AETHERBALL.grid_start.y) % patternHeight;
+		return CONFIG.AETHERBALL.grid_pattern[mappedY][mappedX];
+	}
+}
+
+const { ApplicationV2: ApplicationV2$1, HandlebarsApplicationMixin: HandlebarsApplicationMixin$1 } = foundry.applications.api;
 
 class AetherballPopout extends HandlebarsApplicationMixin$1(ApplicationV2$1) {
 	static DEFAULT_OPTIONS = {
@@ -52,22 +76,37 @@ class AetherballPopout extends HandlebarsApplicationMixin$1(ApplicationV2$1) {
 
     async _renderFrame(options) {
 		const frame = await super._renderFrame(options);
-		this.window.close.remove(); // Prevent closing
+		this.window.close.remove();
 		return frame;
 	}
 
     async close(options={}) {
-		if ( !options.closeKey ) return super.close(options);
+		if (!options.closeKey) return super.close(options);
 		return this;
+	}
+
+	async update() {
+		if(canvas.tokens.controlled.length !== 1) {
+			await this.close({ animate: false })
+			return
+		}
+		
+		CONFIG.AETHERBALL.popout.current_tile = validateTokenTile(canvas.tokens.controlled[0])
+		if(CONFIG.AETHERBALL.popout.current_tile != ACTIVE_TILE.Invalid) await this.render(true);
+		else await this.close({ animate: false })
 	}
 
 	_onRender(context, options) {
 		super._onRender(context, options);
+		this.position.width = "auto";
+		this.position.top = canvas.screenDimensions[1] - 250
+		//this.element.style.width = "auto";
+		console.log(this.position)
 		for (const button of this.element.querySelectorAll(".aetherball-popout button")) {
 			button.addEventListener("click", async (e) => {
 				const selectedToken = canvas.tokens.controlled[0];
-				if (!selectedToken) {
-					ui.notifications.warn("Please select a token first.");
+				if (!selectedToken || canvas.tokens.controlled.length > 1) {
+					ui.notifications.warn("Please select only one token first.");
 					return;
 				}
 				const action = await fetchAction(e.currentTarget.dataset.action);
@@ -93,13 +132,12 @@ class AetherballPopout extends HandlebarsApplicationMixin$1(ApplicationV2$1) {
 	}
 
 	async _prepareContext(_options) {
-		const actions = await fetchActions();
+		const actions = await fetchAllActions();
 		
 		let display = []
 
 		for(let action in actions) {
-			//if(!TILE_MAP[CONFIG.AETHERBALL.state].includes(actions[action].name)) continue
-			if(actions[action].type == "effect") continue
+			if(!TILE_MAP[CONFIG.AETHERBALL.popout.current_tile].includes(actions[action].name)) continue
 			display.push({
 				"id": actions[action].id,
 				"img": actions[action].img,
@@ -129,7 +167,7 @@ class AetherballPopout extends HandlebarsApplicationMixin$1(ApplicationV2$1) {
 	}
 }
 
-async function fetchActions() {
+async function fetchAllActions() {
 	const compendium = game.packs.get('aetherball.aetherball');
 	const actions = await compendium.getDocuments();
 	return actions;
@@ -142,30 +180,22 @@ async function fetchAction(itemId) {
 }
 
 async function preloadTemplates() {
-	const templatePaths = [
+	return loadTemplates([
 		"modules/aetherball/templates/popout.html",
-	];
-
-	return loadTemplates(templatePaths);
+	]);
 }
 
 Hooks.once('init', async function() {
 	preloadTemplates();
-    CONFIG.AETHERBALL = new Aetherball()
 	registerSettings();
+    CONFIG.AETHERBALL = new Aetherball()
 	registerKeybindings();
 });
 
 Hooks.once('ready', async function() {
-    //if (game.settings.get("aetherball", "autoOpenPopout")) togglePopout();
-    togglePopout();
-});
-
-async function togglePopout() {
 	CONFIG.AETHERBALL.popout ??= new AetherballPopout();
-	if (CONFIG.AETHERBALL.popout.rendered) await CONFIG.AETHERBALL.popout.close({ animate: false });
-	else await CONFIG.AETHERBALL.popout.render(true);
-}
+    CONFIG.AETHERBALL.popout.update()
+});
 
 function registerSettings() {
 	game.settings.register("aetherball", "popoutPosition", {
@@ -184,3 +214,21 @@ function registerKeybindings() {
 		}
 	});
 }
+
+Hooks.on('controlToken', (e) => {
+	CONFIG.AETHERBALL.popout.update()
+})
+
+Hooks.on('updateToken', (e) => {
+	const selectedToken = canvas.tokens.controlled[0];
+	if(e.id != selectedToken.id) return
+	CONFIG.AETHERBALL.popout.update()
+});
+
+Hooks.on('updateCombat', (e) => {
+	CONFIG.AETHERBALL.popout.update()
+})
+
+Hooks.on('deleteCombat', (e) => {
+	CONFIG.AETHERBALL.popout.update()
+})
